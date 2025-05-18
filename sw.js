221971fee7debec09b7fbfc31978076628fab110
shintopc <50198933+shintopc@users.noreply.github.com>
2025-05-18 18:15:33 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const cacheName = 'v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
