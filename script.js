a9cca6f1531aae4541af22534d58400cbd564d4b
shintopc <50198933+shintopc@users.noreply.github.com>
2025-05-18 18:14:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -154,3 +154,13 @@ setInterval(updateClock, 1000);
 
 // Initial search
 showResults();
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js')
+      .then(reg => console.log('Service Worker registered', reg))
+      .catch(err => console.error('Service Worker registration failed', err));
+  });
+}
+
